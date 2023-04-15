2c031a552713e3b4bcf65bc3f4f7713544cbc1ed
erzajuan <direkal2@gmail.com>
2023-04-15 22:57:22 +0700

    Fixing Merge

--- server/app.js
+++ server/app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const bodyParser = require("body-parser");
+var cors = require('cors')
 
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/assets", express.static("assets"));
